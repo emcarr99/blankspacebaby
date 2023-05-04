d6a5dc7c44728b96738cbafddf4782c24127010b
Emma Carr <lorenagracex@gmail.com>
2023-05-03 20:29:30 -0500

    added all necessary elements to server i think

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 // connect to route files
-const routDir = require('./routes/index');
+const routeDir = require('./routes/index');
 const api = require('./routes/api');
 
 // turn on express
@@ -13,11 +13,18 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // urlencoded/JSON form data
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// GET route homepage
+app.use('/api', api);
+app.use('/', routeDir);
 
+// GET route homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html')));
+
+  
 // listener verify PORT
 app.listen(PORT, () => {
   console.log(`Live on ${PORT}!`);
