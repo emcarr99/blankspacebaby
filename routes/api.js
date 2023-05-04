e88e5b822cd317421bf326335941967c19ed0b51
Emma Carr <lorenagracex@gmail.com>
2023-05-04 18:01:16 -0500

    psuedo code for api routes

---
+++ routes/api.js
@@ -0,0 +1,9 @@
+// dependencies for middleware
+
+// render saved notes from json file
+
+// display specific note by id and read them -> convert json
+
+// remove individual notes by id uuid
+
+// export router
