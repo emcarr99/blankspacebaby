3944616dc948abf4f7c252df0bb0c33cbb293368
Emma Carr <lorenagracex@gmail.com>
2023-05-03 19:59:41 -0500

    pseudo code server file and fixed folders

---
+++ server.js
@@ -0,0 +1,13 @@
+// add dependencies 
+
+// connect to route files
+
+// turn on express
+
+// set port to local host or heroku choice
+
+// urlencoded/JSON form data
+
+// GET route homepage
+
+// listener verify PORT
