6fe1a5f9226c811bf5591a68b5a3def37b3b3bff
Emma Carr <lorenagracex@gmail.com>
2023-05-03 20:18:55 -0500

    mapping out what files are needed

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 // add dependencies 
+const express = require('express');
+const path = require('path');
 
 // connect to route files
+const routDir = require('./routes/index');
+const api = require('./routes/api');
 
 // turn on express
+const app = express();
 
 // set port to local host or heroku choice
+const PORT = process.env.PORT || 3001;
 
 // urlencoded/JSON form data
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET route homepage
 
 // listener verify PORT
+app.listen(PORT, () => {
+  console.log(`Live on ${PORT}!`);
+});
