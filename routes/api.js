c517457fdaf80c8dbfca9eda6a4a9cde0263868a
Emma Carr <lorenagracex@gmail.com>
2023-05-04 18:13:33 -0500

    router to get note by id

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,23 @@
 // dependencies for middleware
-
+const router = require('express').Router();
+const {v4: uuidv4} = require('uuid');
+const {readNotes, writeNote, readAndWrite} = require('../lib/notes');
+const { json } = require('express');
 // render saved notes from json file
-
+router.get('/notes', (req, res) => {
+  readNotes('./db/db.json')
+  .then((notes) => res.json(JSON.parse(notes)))
+});
 // display specific note by id and read them -> convert json
+router.get('/notes:id', (req, res) => {
+  const nID = req.params.id;
+  readNotes('./db/db/.json')
+  .then((notes) => JSON.parse(notes))
+  .then((json) => {
+    const result = json.filter((note) => note.id === nID)
+    return res.json(result); 
+  });
+});
 
 // remove individual notes by id uuid
 
