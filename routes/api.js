7f14b9443e90fd20918964f82fdf75f8c9ea3a73
Emma Carr <lorenagracex@gmail.com>
2023-05-04 19:24:36 -0500

    IT WORKS THANK U

--- routes/api.js
+++ routes/api.js
@@ -19,6 +19,36 @@ router.get('/notes:id', (req, res) => {
   });
 });
 
+router.post('/notes', (req, res) => {
+  const {title, text} = req.body
+
+  const newNote = {
+    title,
+    text,
+    id: uuidv4()
+  };
+
+  readAndWrite(newNote, './db/db.json');
+  res.json(newNote);
+
+  console.log(newNote + 'new note added');
+});
+
 // remove individual notes by id uuid
+router.delete("/notes/:id", (req, res) => {
+  console.log("Note successfully deleted");
+  const noteId = req.params.id;
+  readNotes("./db/db.json")
+    .then((notes) => JSON.parse(notes))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
+
+      writeNote("./db/db.json", result);
+
+      res.json({ message: "deleted" });
+    });
+});
+  
 
 // export router
+module.exports = router;
